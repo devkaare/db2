695fb7bf9241e20448baf3a3c90186c899cefbc8
Kaare <kaarehofferdal@gmail.com>
2024-06-05 22:29:31 +0200

    Update main.go

--- example/main.go
+++ example/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/devkaare/db2"
+	"db-beta/db" // Replace this with the URL for this repo when testing!
 	"log"
 	//"time"
 
@@ -12,8 +12,7 @@ var userKey = "Users"
 
 func main() {
 	db.LoadCache("users.json") // Loading the users.json files contents into memory and creating a users.json file if none exists
-	defer db.SaveCache() // Ensuring that the program saves the user cache to the json file on shutdown
-	// Important: The two functions above are CRUCIAL for this package to function!
+	defer db.SaveCache() // Ensuring that the program saves the user cache to the json file on shutdown, This can also be done after every save operation aswell
 
 	userId := uuid.New().String()
 	newUser := CreateUser(userId, "Emma A. Davis", "password2", "emma@example.com2")
@@ -22,16 +21,24 @@ func main() {
 	db.AddToCache(userKey, newUser)
 	log.Println("Added user:", newUser)
 
-	userIdToDelete := "ee3bebf0-421d-4a9f-9a01-fd12ccb8805f" // Important: Replace this with an UserId from db.json to test
+	userIdToDelete := userId // Important: Replace this with an UserId from db.json to test
 	user := db.SearchCache(userKey, "UserId", userIdToDelete)
 
 	if user != nil {
 		log.Println("User found:", user)
+        // Get user Id using the key "UserId"
 		log.Println("User Id:", user["UserId"])
+        // Delete from cache
 		db.DeleteFromCache(userKey, "UserId", userIdToDelete)
 	} else {
 		log.Println("User with Id", userIdToDelete, "does not exist.")
 	}
+    
+    // Get full user cache
+    cache := db.GetCache(userKey)
+    for _, users := range cache {
+        log.Println("User:", users)
+    }
 }
 
 func CreateUser(userId, username, password, email string) map[string]interface{} {
